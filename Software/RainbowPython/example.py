be7070c56e1014fdebd45148e78d377c595f809f
sqozz <sqozz@geekify.de>
2015-03-23 20:17:39 +0100

    added some timeout to the RainbowPython example

--- Software/RainbowPython/example.py
+++ Software/RainbowPython/example.py
@@ -1,12 +1,15 @@
 #!/bin/env python3
 # -*- coding: utf-8 -*-
 from RainbowPython import Rainbow
+from time import sleep
 
 rbp = Rainbow()
 while True:
 	for i in range(1, 256):
 		rbp.setColorRGB(i, i, i)
-	
+		sleep(0.01)
+
 	for i in range(1, 256):
 		i = 256 - i
 		rbp.setColorRGB(i, i, i)
+		sleep(0.01)
